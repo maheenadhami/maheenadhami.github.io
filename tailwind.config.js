d6c4788549425620e238f9ad269d200dc3dddc88
maheenadhami <madhami@edu.uwaterloo.ca>
2021-01-18 03:19:26 -0500

    updating site

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'alice': ['Alice', 'serif'],
+        'opens':['Open+Sans','sans-serif']
+      }, 
+      colors: {
+        'salmon': '#FE938C',
+        'navy-1': '#3A5E72',
+        'blue-1': '#4281A4',
+        'mustard': '#EAD2AC',
+        'peach': '#E5D4C8',
+      }
+    },
+  },
+  variants: {
+  },
+  plugins: [],
+}
